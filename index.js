4ff392fd3f33f63a9386c4f2b5d0000ea696f4cb
Jane Weesner <jane.e.weesner@gmail.com>
2018-07-09 15:10:05 -0700

    Add ability to set own dev key and (buggy) ability to get more than 30 records Change file name written to be reference to id and prevent overwrite when running multiple times. correct number of file downloads made, but api gives many duplicates, not true random. may need to run multiple times for desired number of records.

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ var fs = require('fs');
 var https = require("https")
 var path = require("path")
 var program = require("commander")
+var ProgressBar = require('progress');
 
 program
   .version('1.0.0')
-  .option('--amount [limit]', 'Amount of pictures to download (Default is 10)')
+  .option('--amount [limit]', 'Amount of pictures to download (Default is 10), Max 100, unless using personal dev key')
   .option('--folder [name]', 'Name of the folder you want to save the images to (Default is "images")')
   .option('--width [w]', 'Width of images (Default is 1200)')
   .option('--height [h]', 'Height of images (Default is 800)')
   .option('--featured [f]', 'Download featured images only')
   .option('--search [s]', 'Download images with a specific term')
   .option('--orientation [o]', 'Image orientation (landscape, portrait, and squarish)')
+  .option('--devKey [k]', 'Developer Key to use for request. Amount parameter limited if not set')
   .parse(process.argv);
 
 if(!program.amount){
@@ -24,33 +26,50 @@ if(!program.folder){
 }
 
 if(!program.orientation){
-    program.orientation= "landscape"
+    program.orientation = "landscape"
 }
+
+if (!program.devKey){
+    program.devKey = "1ced621f3ac7bb7836fc9b6bfbcff5656dd43eb2b60f4636e5ef53142ea19f2d"
+    if (program.amount > 100){
+        program.amount = 100;
+    }
+}
+
 
 console.log("\nWelcome to Bulksplash! (Powered by Unsplash.com)")
 
 console.log("\nDownloading " + program.amount + " random images :)")
 
-var ProgressBar = require('progress');
+
 var bar = new ProgressBar(':bar', { total: parseInt(program.amount) });
+var dupe = 0;
 
 function download(url, dest, dirname) {
     var dir = './' + dirname;
-
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir)
-    }
-
-    var file = fs.createWriteStream(dest);
-    var request = https.get(url, function(response) {
-        response.pipe(file);
-        bar.tick();
-    }).on("error", function (e) {
-        console.log("Error while downloading", url, e.code);
+    var requestPromise = new Promise ((resolve, reject) => {
+        https.get(url, function(response) {
+            if (!fs.existsSync(dir)){
+                fs.mkdirSync(dir)
+            }
+            if (fs.existsSync(dest)){
+                dupe++;
+                console.log(dupe)
+            }
+            var file = fs.createWriteStream(dest);
+            response.pipe(file)
+            bar.tick();
+            resolve();
+        }).on("error", function (e) {
+            console.log("Error while downloading", url, e.code);
+            reject(e);
+        });
     });
-};
+    return requestPromise;
 
-var url = "https://api.unsplash.com/photos/random?count=" + program.amount + "&orientation=" + program.orientation + "&client_id=1ced621f3ac7bb7836fc9b6bfbcff5656dd43eb2b60f4636e5ef53142ea19f2d";
+};
+var downloadsRemaining = program.amount;
+var url = `https://api.unsplash.com/photos/random?count=${downloadsRemaining <=30 ? downloadsRemaining : 30 }&orientation=${program.orientation}&client_id=${program.devKey}`;
 
 if(program.width){
     var url = url + "&w=" + program.width;
@@ -65,23 +84,31 @@ if(program.search){
 }
 
 console.log("\nDownloading images from:\n")
-request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        var body = JSON.parse(body);
-
-        
-        for (i in body){
-            if(program.width || program.height){
-                var img = body[i]["urls"].custom
-            } else{
-                var img = body[i]["urls"].raw
+function getAndDownloadImgs (){
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            var body = JSON.parse(body);
+            var downloadPromises = [];
+            
+            for (i in body){
+                if(program.width || program.height){
+                    var img = body[i]["urls"].custom
+                } else{
+                    var img = body[i]["urls"].raw
+                }
+    
+                console.log(body[i]["user"].name + " (" + body[i]["user"].links["html"] + ")")
+                downloadPromises.push(download(img, path.join(__dirname, "/" + program.folder + "/image-" + body[i]['id'] + ".jpg"), program.folder))
             }
-
-            console.log(body[i]["user"].name + " (" + body[i]["user"].links["html"] + ")")
-            download(img, path.join(__dirname, "/" + program.folder + "/image-" + i + ".jpg"), program.folder)
+            Promise.all(downloadPromises).then(()=>{
+                downloadsRemaining -= 30;
+                if (downloadsRemaining > 0){
+                    getAndDownloadImgs();
+                }
+            }).catch(err => console.log(err));
+        } else {
+            console.log("Got an error: ", error)
         }
-
-    } else {
-        console.log("Got an error: ", error)
-    }
-})
+    })
+}
+getAndDownloadImgs(); 
